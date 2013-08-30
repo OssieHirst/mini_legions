506d0d37c13ba7483f1cd30c86d8ae064f0b9354
WorldOfProper <worldofproper@gmail.com>
2013-08-30 19:27:21 +0100

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Olorin Istar"
+    email    "secretfire81@gmail.com"
+    password "cheers1erm"
+    password_confirmation "cheers1erm"
+  end
+end
