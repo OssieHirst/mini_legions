5e7c341e5b0793d2df404da1bf17ad20fbe5aeeb
WorldOfProper <worldofproper@gmail.com>
2013-10-31 12:54:58 +0000

    Fixing AWS stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,9 @@ MiniLegions::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['minilegions'],
-    :access_key_id => ENV['AKIAISXNMHPOPOLTCQ2A'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
