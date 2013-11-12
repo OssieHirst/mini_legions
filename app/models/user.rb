ddb0b6876173bb22b10bbf3baa6421bbd0faee47
WorldOfProper <worldofproper@gmail.com>
2013-11-12 19:28:10 +0000

    Added unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@ class User < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :username, presence: true,
+                       length: { maximum: 15 },
+                       uniqueness: { case_sensitive: false }                  
   has_secure_password
   validates :password, length: { minimum: 6 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024151223) do
+ActiveRecord::Schema.define(version: 20131112185206) do
 
   create_table "collections", force: true do |t|
     t.integer  "user_id"
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 20131024151223) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
