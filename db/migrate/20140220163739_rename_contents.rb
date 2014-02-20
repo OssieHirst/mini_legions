9c53671b21ee569f88a47a4a3c5e31ebcb18736e
WorldOfProper <worldofproper@gmail.com>
2014-02-20 23:53:55 +0000

    fixed migrations for heroku

--- db/migrate/20140220163739_rename_contents.rb
+++
@@ -1,5 +0,0 @@
-class RenameContents < ActiveRecord::Migration
-  def change
-  	rename_table :content, :contents
-  end
-end
--- db/migrate/20140220164126_rename_content.rb
+++
@@ -1,5 +0,0 @@
-class RenameContent < ActiveRecord::Migration
-  def change
-  	rename_table :content, :contents
-  end
-end
