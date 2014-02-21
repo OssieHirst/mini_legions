02780831db8c4a44e55e04497fb52115a6b984a8
WorldOfProper <worldofproper@gmail.com>
2014-02-21 16:07:14 +0000

    Disabled autocomplete on contents new form

--- app/helpers/miniatures_helper.rb
+++ app/helpers/miniatures_helper.rb
@@ -1,2 +1,7 @@
 module MiniaturesHelper
+	def miniset_link_with_quantity(miniset)
+  		string = link_to content.miniset.name, content.miniset
+  		string << "(x#{content.quantity})" if content.quantity.present?
+  		return string
+end
 end
