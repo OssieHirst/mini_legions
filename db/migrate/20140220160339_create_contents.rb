f10b9c57b0928a7c1b4c99796fa50da2b860b2cc
WorldOfProper <worldofproper@gmail.com>
2014-03-06 18:24:10 +0000

    Hoping to fix contents table

--- db/migrate/20140220160339_create_contents.rb
+++ db/migrate/20140220160339_create_contents.rb
@@ -2,7 +2,7 @@ class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
       t.integer :miniset_id
-      t.integer :miniature_id
+      t.integer :setmini_id
       t.integer :quantity
 
       t.timestamps
