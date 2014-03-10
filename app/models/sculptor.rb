e4c6dd5b5f48ff9099b0feb113ee977dc68cbdaf
WorldOfProper <worldofproper@gmail.com>
2014-03-10 19:20:27 +0000

    Replaced sculptor first name iwth initial in indexes

--- app/models/sculptor.rb
+++ app/models/sculptor.rb
@@ -1,7 +1,12 @@
 class Sculptor < ActiveRecord::Base
 	has_many :sculptings
 	has_many :miniatures, :through => :sculptings
+
 	def fullname
   	  "#{first_name} #{last_name}"
+    end
+
+    def initial_fullname
+    	"#{first_name[0,1]}. #{last_name}"
     end
 end
