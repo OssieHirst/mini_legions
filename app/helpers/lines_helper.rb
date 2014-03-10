a7d22d4f1e82bf04759ce23265902fce70ab46f3
WorldOfProper <worldofproper@gmail.com>
2014-03-10 18:25:17 +0000

    Added miniature count to lines and mfr show pages

---
+++ app/helpers/lines_helper.rb
@@ -0,0 +1,14 @@
+module LinesHelper
+
+	def line_sublink_with_count(line)
+  		string = link_to line.name, line
+  		string << " (#{Miniature.uniq.joins(:lines).where(line.subtree_conditions).count})" if Miniature.uniq.joins(:lines).where(line.subtree_conditions).any?
+  		return string
+	end
+
+	def line_count
+		string = " (#{Miniature.uniq.joins(:lines).where(@line.subtree_conditions).count})" if Miniature.uniq.joins(:lines).where(@line.subtree_conditions).any?
+  		return string
+	end
+
+end
