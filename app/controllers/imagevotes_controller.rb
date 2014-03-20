5a9cbe49e548526af104c16ae4a21b052ea9c608
WorldOfProper <worldofproper@gmail.com>
2014-03-20 20:05:53 +0000

    Gote image vote working

--- app/controllers/imagevotes_controller.rb
+++ app/controllers/imagevotes_controller.rb
@@ -7,13 +7,15 @@ class ImagevotesController < ApplicationController
 		@miniature = Miniature.find(params[:miniature_id])
 	end
 
-	def edit
+	def show
     	@imagevote = Imagevote.find_by_miniature_id_and_voter_id(params[:miniature_id],params[:voter_id]) 
     	@miniature = Miniature.find(params[:miniature_id]) 
     end
 
 	def create
   		@imagevote = Imagevote.new(imagevote_params)
+  		@collection = Collection.find(params[:imagevote][:collection_id])
+  		@imagevote.voted_id = @collection.user_id
   		if @imagevote.save
   			flash[:success] = "Vote registered"
   			redirect_to :back
@@ -25,6 +27,8 @@ class ImagevotesController < ApplicationController
 
 	def update
 		@imagevote = Imagevote.find(params[:id])
+		@collection = Collection.find(params[:imagevote][:collection_id])
+  		@imagevote.voted_id = @collection.user_id
 		if @imagevote.update_attributes(imagevote_params)
 			flash[:success] = "Vote changed."
       		redirect_to :back
@@ -36,6 +40,6 @@ class ImagevotesController < ApplicationController
 	private
 
     def imagevote_params
-      params.require(:imagevote).permit(:collection_id, :voter_id, :voted_id, :miniature_id)
+      params.require(:imagevote).permit(:collection_id, :voter_id, :miniature_id)
     end
 end
