c12809958e47de5400c6d62012a50193161f821d
WorldOfProper <worldofproper@gmail.com>
2014-03-20 20:07:48 +0000

    Image vote working but without counter caches

--- app/helpers/manufacturers_helper.rb
+++
@@ -1,9 +0,0 @@
-module ManufacturersHelper
-
-	def line_sublink_with_count(line)
-  		string = link_to line.name, line
-  		string << " (#{Miniature.uniq.joins(:lines).where(line.subtree_conditions).count})" if Miniature.uniq.joins(:lines).where(line.subtree_conditions).any?
-  		return string
-	end
-
-end
--- db/migrate/20140306162921_fix_contents_names.rb
+++
@@ -1,5 +0,0 @@
-class FixContentsNames < ActiveRecord::Migration
-  def change
-  	rename_column :contents, :miniature_id, :setmini_id
-  end
-end
