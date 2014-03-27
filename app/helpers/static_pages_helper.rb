fa3232fb2c49a3ae065d672f51cf2b4a0d6f0822
WorldOfProper <worldofproper@gmail.com>
2014-03-27 16:19:35 +0000

    Styled user stats

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
 	def picfeed
-		pics = Collection.where('photo_updated_at >= ?', 1.day.ago)
+		pics = Collection.where('photo_updated_at >= ?', 1.week.ago)
     	pics.take(5)
     end
 end
