e844faeb182deda81855501c805446329863b081
WorldOfProper <worldofproper@gmail.com>
2014-04-03 15:35:32 +0100

    Extended picfeed length to a month

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
 	def picfeed
-		pics = Collection.where('photo_updated_at >= ?', 1.week.ago)
+		pics = Collection.where('photo_updated_at >= ?', 1.month.ago)
     	pics.take(5)
     end
 
