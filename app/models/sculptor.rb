8aa308a990e092af6227fa16e6879a8590caad60
WorldOfProper <worldofproper@gmail.com>
2014-04-06 14:53:33 +0100

    Sculptor sort order to last_name ASC

--- app/models/sculptor.rb
+++ app/models/sculptor.rb
@@ -1,6 +1,7 @@
 class Sculptor < ActiveRecord::Base
 	has_many :sculptings
 	has_many :miniatures, :through => :sculptings
+	default_scope order('last_name ASC')
 
 	def fullname
   	  "#{first_name} #{last_name}"
