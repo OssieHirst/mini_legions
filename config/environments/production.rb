4295ee427402d9e678c5cf75bf132d4aeebd89ba
WorldOfProper <worldofproper@gmail.com>
2014-06-13 14:45:53 +0100

    Fixed asset link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ MiniLegions::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-   config.action_controller.asset_host = "https://s3.amazonaws.com/minilegions/"
+   config.action_controller.asset_host = "https://s3.amazonaws.com/minilegions"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
