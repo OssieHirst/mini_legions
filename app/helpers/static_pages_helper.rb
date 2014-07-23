26059085b7ba44a81aaba58734bd6b7635b1b52e
WorldOfProper <worldofproper@gmail.com>
2014-07-23 12:14:55 +0100

    Extended home picfeed to 5 days

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
 	def picfeed
-		pics = Collection.where('photo_updated_at >= ?', 3.days.ago)
+		pics = Collection.where('photo_updated_at >= ?', 5.days.ago)
     	pics.take(8)
     end
 
