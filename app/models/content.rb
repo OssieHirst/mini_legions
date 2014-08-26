c6b47b8314d1677b6583b4ef582d5b829b2a98b1
WorldOfProper <worldofproper@gmail.com>
2014-08-26 15:44:25 +0100

    Fixing Contents read only bug

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ActiveRecord::Base
-	default_scope joins(:setmini).order('miniatures.name ASC')
+	default_scope joins(:setmini).order('miniatures.name ASC').readonly(false)
 	belongs_to :miniset, class_name: "Miniature"
 	belongs_to :setmini, class_name: "Miniature"
 	validates :miniset_id, presence: true
