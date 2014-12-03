9a9db027a038d209697964bb3d224d2988ea40e2
WorldOfProper <ossiehirst@me.com>
2014-12-03 17:48:22 +0000

    Removed titleize from Miniatures.name

--- app/models/miniature.rb
+++ app/models/miniature.rb
@@ -42,9 +42,7 @@ class Miniature < ActiveRecord::Base
 
 
   
-  def name=(s)
-    super s.titleize
-  end
+
 
   def self.import(file)
     CSV.foreach(file.path, :encoding => 'windows-1251:utf-8', headers: true) do |row|
